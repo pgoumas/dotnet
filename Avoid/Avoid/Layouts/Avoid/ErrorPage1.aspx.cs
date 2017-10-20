f95d663952322c2519241317f3f4917aab4e4ab9
pgoumas <pgoumas@hotmail.com>
2017-10-20 17:31:01 +0300

    avoid web part

---
+++ Avoid/Avoid/Layouts/Avoid/ErrorPage1.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+
+namespace Avoid.Layouts.Avoid
+{
+    public partial class ErrorPage1 : LayoutsPageBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ Avoid/Avoid/VisualWebPart1/VisualWebPart1UserControl.ascx.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.SharePoint.Utilities;
+using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+
+namespace Avoid.VisualWebPart1
+{
+    public partial class VisualWebPart1UserControl : UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string dummy_query1 = Request.Form["query"];//post
+            string dummy_query2 = Request.QueryString["query"];//get
+            if (!String.IsNullOrEmpty(dummy_query1))
+            {
+                if (dummy_query1.Contains("javascript"))
+                {
+                    Server.Transfer("/_layouts/15/Avoid/ErrorPage1.aspx", true);
+                }
+                if (dummy_query1.Contains("script"))
+                {
+                    Server.Transfer("/_layouts/15/Avoid/ErrorPage1.aspx", true);
+                }
+                //Response.Redirect(Page.ResolveClientUrl(@"~hostUrl\_layouts\Avoid\ErrorPage1.aspx"));
+                //SPUtility.Redirect("settings.aspx", SPRedirectFlags.Static | SPRedirectFlags.RelativeToLayoutsPage, HttpContext.Current);
+                //Response.Redirect("http://www.microsoft.com/gohere/look.htm");
+                //SPUtility.Redirect
+                //Page.Response.Redirect("http://www.microsoft.com/gohere/look.htm");
+                //Context.Reponse.Redirect("http://www.microsoft.com/gohere/look.htm");
+            }
+            if (!String.IsNullOrEmpty(dummy_query2) )
+            {
+                if (dummy_query2.Contains("javascript"))
+                {
+                    Server.Transfer("/_layouts/15/Avoid/ErrorPage1.aspx", true);
+                }
+                if (dummy_query2.Contains("script"))
+                {
+                    Server.Transfer("/_layouts/15/Avoid/ErrorPage1.aspx", true);
+                }
+
+            }
+        }
+    }
+}
